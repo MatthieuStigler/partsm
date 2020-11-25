f342e1966083b6f5d7ce520af0395eab1d4a6a54
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-11-24 19:48:30 -0800

    Add more itnernal tests code lines

--- partsm/inst/internal_tests_devtools.R
+++ partsm/inst/internal_tests_devtools.R
@@ -1,4 +1,23 @@
+
+## Online Checks
 devtools::check_rhub(interactive = FALSE)
 devtools::check_win_devel()
 devtools::check_win_release()
 devtools::check_win_oldrelease()
+
+## 
+# spelling::spell_check_setup(lang = "en-UK")
+devtools::spell_check(use_wordlist = TRUE)
+
+#
+# usethis::use_cran_comments(open = FALSE)
+
+
+
+## reverse dependency? not working well as of Nov 2020
+# usethis::use_revdep()
+# revdepcheck::revdep_check(num_workers = 4)
+
+## Release
+devtools::release_checks()
+devtools::release()
